5091a9007b69119c93465d80a4093a89f1362d92
posaunehm <posaunehm@gmail.com>
2012-05-16 08:45:52 +0900

    気になったところをコメント追記。

--- ProgrammingFsShakyo/Program.fs
+++ ProgrammingFsShakyo/Program.fs
@@ -6,11 +6,20 @@ let main(args : string[]) =
     //引数が2つでなければ
     if args.Length <> 2 then
         //例外を吐いて落ちる
+        //インデントでifのスコープが決定される
         failwith "Error: Expected arguments <greeting> and <thing>"
-    
+    //コンマ区切りでの初期化。F#独特。
     let greeting, thing = args.[0], args.[1]
+    //.NETクラスの呼び出し
     let timeOfDay = DateTime.Now.ToString("hh:mm tt")
     
+    //コメントの別の書き方を試す
+    (*
+    コンソールへ出力
+    Printf.TextWriterFormatのエイリアスっぽい
+    *)
     printfn "%s, %s at %s" greeting thing timeOfDay
+    
+     
     
     0 
