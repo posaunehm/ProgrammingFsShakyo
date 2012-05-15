7d94a27228ba8f5a8929bb9f4f8e2b3403acfe30
posaunehm <posaunehm@gmail.com>
2012-05-16 08:39:13 +0900

    サンプル２を追加。コマンドライン引数を取り、引数の数のチェックをおこなっている

--- ProgrammingFsShakyo/Program.fs
+++ ProgrammingFsShakyo/Program.fs
@@ -1,1 +1,16 @@
-﻿
+﻿open System
+
+//メイン関数
+[<EntryPoint>]
+let main(args : string[]) = 
+    //引数が2つでなければ
+    if args.Length <> 2 then
+        //例外を吐いて落ちる
+        failwith "Error: Expected arguments <greeting> and <thing>"
+    
+    let greeting, thing = args.[0], args.[1]
+    let timeOfDay = DateTime.Now.ToString("hh:mm tt")
+    
+    printfn "%s, %s at %s" greeting thing timeOfDay
+    
+    0 
