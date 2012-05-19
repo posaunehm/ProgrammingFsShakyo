b1ecdee8b174b31c9ddae61571f1c322e55a4ddd
posaunehm <posaunehm@gmail.com>
2012-05-19 13:11:37 +0900

    関数型プログラミングの最初の一歩の部分を写経。パイプ演算子は本当にLinqのメソッド記法っぽい。

---
+++ ProgrammingFsShakyo/Chapter3.fs
@@ -0,0 +1,28 @@
+﻿module ProgrammingFsShakyo.Chapter3
+
+let square x = x * x
+
+//宣言的な加算の書き方
+let imperaticeSum numbers = 
+    //ミュータブル宣言。こういう書き方もあるのね。
+    let mutable total = 0
+    for i in numbers do
+        let x = square i
+        //代入はこう書くのか・・・
+        total <- total + x
+    total
+
+let ans = imperaticeSum [1 .. 10]
+
+//関数型な加算の書き方
+let functionalSum numbers = 
+    numbers
+    //パイプ演算だ！これは関数型プログラミングの範疇で書かれるのね。
+    |> Seq.map square
+    |> Seq.sum
+
+//返り値もmutableだからシャドウイングできない！！ 
+//let ans = functionalSum [1 .. 100]
+let ans2 = functionalSum [1 .. 100]
+
+
