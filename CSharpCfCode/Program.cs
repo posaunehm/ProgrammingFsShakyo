fed17be43e17a98d480f3cd05c97db6b6e1de24f
posaunehm <posaunehm@gmail.com>
2012-05-24 22:40:41 +0900

    C#の参照用コードを追加

---
+++ CSharpCfCode/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CSharpCfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = "C:";
+            var files = Directory.GetFiles(path,"*.*",SearchOption.AllDirectories);
+
+            files.Select(file => new FileInfo(file))
+                    .Select(info => info.Length)
+                    .Sum();
+        }
+    }
+}
