fd2271347d32e369a0c0654c3552b0d67b1b45ef
posaunehm <posaunehm@gmail.com>
2012-05-28 21:39:39 +0900

    部分関数適用した結果を追加。関数を返す関数とごっちゃになりそうだけど（というかカリー化は関数を返す関数の一種だとは思うけれど）

--- ProgrammingFsShakyo/Chapter3.fs
+++ ProgrammingFsShakyo/Chapter3.fs
@@ -1,5 +1,7 @@
 ﻿module ProgrammingFsShakyo.Chapter3
 
+open System.IO
+
 let ExecuteChapter3 = 
 
     let square x = x * x
@@ -28,5 +30,23 @@ let ExecuteChapter3 =
     //匿名関数
     let ret = (fun x -> x + 3) 5
     let ret = List.map (fun i -> i * i ) [1 .. 10]
+
+    //部分関数適用
+    //2つのstirngを引数に持つ関数
+    let appendFile filename (text : string) = 
+        use file = new StreamWriter(filename, true)
+        file.WriteLine(text)
+        file.Close()
+    appendFile @"Log.txt" "Processing Event X..."
+
+    //カリー化を行う。第一引数にのみ引数を割り当て
+    let appendLogFile = appendFile @"Log.txt"
+    appendLogFile "Processing Evenct Y..."
+
+    //ラムダ式を使う
+    List.iter (fun i -> printfn "%d" i) [1 .. 10]
+    //部分関数適用で新たな式を作る
+    List.iter (printfn "%d") [1 .. 10]
+
 
     ()
