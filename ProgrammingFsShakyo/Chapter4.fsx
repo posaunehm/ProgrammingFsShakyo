e8a9cc8b132457299b9cb6b2630916d304277b43
Hiroshi Maekawa <posaunehm@gmail.com>
2012-08-02 08:50:54 +0900

    宣言的プログラミングのデフォルト値チェックまで写経。

---
+++ ProgrammingFsShakyo/Chapter4.fsx
@@ -0,0 +1,15 @@
+﻿//第4章：宣言的プログラミング
+module Chapter4
+
+//default値取得
+let defaultArray = Unchecked.defaultof<System.Array>
+//typeof
+let typeOfInt = typeof<int>
+//sizeof
+let sizeOfInt = sizeof<int>
+
+//nullの使用
+let isNull = function null -> true | _ -> false
+isNull "文字列"
+isNull (null : string)
+
